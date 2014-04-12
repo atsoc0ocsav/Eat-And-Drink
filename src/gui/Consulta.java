de9dda581fe400f88c9b1d25c8030c61d0de732c
atsoc0ocsav@gmail.com <atsoc0ocsav@gmail.com@2edde95b-6862-c577-c830-67069ed00eb0>
2014-04-12 17:34:02 +0000

    GUI Changes: - Aligned some components - Added Set Look and Feel (UIManager) - Added JPanel labels
    GUI Code:
    - Organized code

--- src/gui/Consulta.java
+++ src/gui/Consulta.java
@@ -1,29 +1,29 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.awt.Button;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.Label;
-import java.awt.Panel;
 import java.awt.TextField;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
 
 import com.jgoodies.forms.factories.DefaultComponentFactory;
 
 public class Consulta extends JFrame {
 
+	private static final long serialVersionUID = 4213975954950138529L;
 	private JPanel contentPane;
-	private JTextField txtEatDrink;
 
 	/**
 	 * Launch the application.
@@ -43,120 +43,135 @@ public class Consulta extends JFrame {
 
 	/**
 	 * Create the frame.
+	 * 
+	 * @throws UnsupportedLookAndFeelException
+	 * @throws IllegalAccessException
+	 * @throws InstantiationException
+	 * @throws ClassNotFoundException
 	 */
-	public Consulta() {
+	public Consulta() throws ClassNotFoundException, InstantiationException,
+			IllegalAccessException, UnsupportedLookAndFeelException {
+		UIManager.setLookAndFeel(UIManager
+				.getCrossPlatformLookAndFeelClassName());
+
 		setTitle("Eat & Drink Estabelecimentos - Consulta");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 880, 523);
+		setBounds(100, 100, 816, 523);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JLabel lblEatDrink = DefaultComponentFactory.getInstance().createTitle(
-				"Eat and Drink");
-		lblEatDrink.setBounds(119, 11, 632, 31);
-		lblEatDrink.setHorizontalAlignment(SwingConstants.CENTER);
-		lblEatDrink.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		contentPane.add(lblEatDrink);
+		JPanel panel_Filtros = new JPanel();
+		panel_Filtros.setBounds(192, 72, 559, 20);
+		contentPane.add(panel_Filtros);
+		panel_Filtros.setLayout(new GridLayout(1, 6, 0, 0));
 
-		Panel panelFiltros = new Panel();
-		panelFiltros.setBounds(192, 72, 559, 20);
-		contentPane.add(panelFiltros);
-		panelFiltros.setLayout(new GridLayout(1, 6, 0, 0));
+		JLabel lbl_Cidade = new JLabel("CIDADE");
+		lbl_Cidade.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_Filtros.add(lbl_Cidade);
 
-		JLabel lblCidade = new JLabel("CIDADE");
-		lblCidade.setHorizontalAlignment(SwingConstants.CENTER);
-		panelFiltros.add(lblCidade);
+		JComboBox comboBox_Cidade = new JComboBox();
+		panel_Filtros.add(comboBox_Cidade);
 
-		JComboBox comboBoxCidade = new JComboBox();
-		panelFiltros.add(comboBoxCidade);
+		JLabel lbl_Zona = new JLabel("ZONA");
+		lbl_Zona.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_Filtros.add(lbl_Zona);
 
-		JLabel lblZona = new JLabel("ZONA");
-		lblZona.setHorizontalAlignment(SwingConstants.CENTER);
-		panelFiltros.add(lblZona);
+		JComboBox comboBox_Zona = new JComboBox();
+		panel_Filtros.add(comboBox_Zona);
 
-		JComboBox comboBoxZona = new JComboBox();
-		panelFiltros.add(comboBoxZona);
+		JLabel lbl_Tipo = new JLabel("TIPO");
+		lbl_Tipo.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_Filtros.add(lbl_Tipo);
 
-		JLabel lblTipo = new JLabel("TIPO");
-		lblTipo.setHorizontalAlignment(SwingConstants.CENTER);
-		panelFiltros.add(lblTipo);
+		JComboBox comboBox_Tipo = new JComboBox();
+		panel_Filtros.add(comboBox_Tipo);
 
-		JComboBox comboBoxTipo = new JComboBox();
-		panelFiltros.add(comboBoxTipo);
-
-		Panel panelTipoEventos = new Panel();
-		panelTipoEventos.setBounds(192, 126, 319, 157);
-		contentPane.add(panelTipoEventos);
-		panelTipoEventos.setLayout(new BorderLayout(0, 0));
-
-		Panel panel = new Panel();
-		panelTipoEventos.add(panel, BorderLayout.SOUTH);
-		panel.setLayout(new GridLayout(0, 2, 0, 0));
-
-		TextField textFieldEventos = new TextField();
-		panel.add(textFieldEventos);
-
-		Button buttonAddEventos = new Button("Adicionar");
-		panel.add(buttonAddEventos);
+		JPanel panel_TipoEventos = new JPanel();
+		panel_TipoEventos.setBounds(150, 126, 319, 170);
+		contentPane.add(panel_TipoEventos);
+		panel_TipoEventos.setLayout(new BorderLayout(0, 0));
+		panel_TipoEventos.setBorder(BorderFactory
+				.createTitledBorder("Tipo de Eventos"));
 
 		JScrollPane scrollPaneEventos = new JScrollPane();
-		panelTipoEventos.add(scrollPaneEventos, BorderLayout.CENTER);
+		panel_TipoEventos.add(scrollPaneEventos, BorderLayout.CENTER);
 
 		JPanel panel_InScrollPaneEventos = new JPanel();
 		scrollPaneEventos.setViewportView(panel_InScrollPaneEventos);
 		panel_InScrollPaneEventos.setLayout(new GridLayout(0, 2, 0, 0));
 
-		Panel panelTipoPratos = new Panel();
-		panelTipoPratos.setBounds(517, 126, 310, 157);
-		contentPane.add(panelTipoPratos);
-		panelTipoPratos.setLayout(new BorderLayout(0, 0));
+		JPanel panel_TipoEventos_Sul = new JPanel();
+		panel_TipoEventos.add(panel_TipoEventos_Sul, BorderLayout.SOUTH);
+		panel_TipoEventos_Sul.setLayout(new GridLayout(0, 2, 0, 0));
 
-		Panel panel_1 = new Panel();
-		panelTipoPratos.add(panel_1, BorderLayout.SOUTH);
-		panel_1.setLayout(new GridLayout(0, 2, 0, 0));
+		TextField textField_Eventos = new TextField();
+		panel_TipoEventos_Sul.add(textField_Eventos);
 
-		TextField textFieldPratos = new TextField();
-		panel_1.add(textFieldPratos);
+		JButton button_AddEventos = new JButton("Adicionar");
+		panel_TipoEventos_Sul.add(button_AddEventos);
 
-		Button buttonAddPratos = new Button("Adicionar");
-		panel_1.add(buttonAddPratos);
+		JPanel panel_TipoPratos = new JPanel();
+		panel_TipoPratos.setBounds(472, 126, 319, 170);
+		contentPane.add(panel_TipoPratos);
+		panel_TipoPratos.setLayout(new BorderLayout(0, 0));
+		panel_TipoPratos.setBorder(BorderFactory
+				.createTitledBorder("Tipo de Pratos"));
 
 		JScrollPane scrollPanePratos = new JScrollPane();
-		panelTipoPratos.add(scrollPanePratos, BorderLayout.CENTER);
+		panel_TipoPratos.add(scrollPanePratos, BorderLayout.CENTER);
 
 		JPanel panel_InScrollPanelPratos = new JPanel();
 		scrollPanePratos.setViewportView(panel_InScrollPanelPratos);
 		panel_InScrollPanelPratos.setLayout(new GridLayout(1, 0, 0, 0));
 
-		Button buttonAplicarFiltro = new Button("Aplicar Filtro");
-		buttonAplicarFiltro.setBounds(32, 312, 91, 22);
-		contentPane.add(buttonAplicarFiltro);
+		JPanel panel_TipoPratos_Sul = new JPanel();
+		panel_TipoPratos.add(panel_TipoPratos_Sul, BorderLayout.SOUTH);
+		panel_TipoPratos_Sul.setLayout(new GridLayout(0, 2, 0, 0));
 
-		Button buttonFiltroTodos = new Button("Todos");
-		buttonFiltroTodos.setBounds(137, 312, 80, 22);
-		contentPane.add(buttonFiltroTodos);
+		TextField textField_Pratos = new TextField();
+		panel_TipoPratos_Sul.add(textField_Pratos);
 
-		TextField textFieldAvaliacao = new TextField();
-		textFieldAvaliacao.setBounds(83, 145, 38, 22);
-		contentPane.add(textFieldAvaliacao);
+		JButton button_AddPratos = new JButton("Adicionar");
+		panel_TipoPratos_Sul.add(button_AddPratos);
 
-		Label label = new Label("AVALIA\u00C7\u00C3O");
-		label.setBounds(65, 117, 80, 22);
-		contentPane.add(label);
+		JButton button_AplicarFiltro = new JButton("Aplicar Filtro");
+		button_AplicarFiltro.setBounds(32, 312, 91, 22);
+		contentPane.add(button_AplicarFiltro);
 
-		Label label_1 = new Label(">=");
-		label_1.setBounds(65, 145, 26, 22);
-		contentPane.add(label_1);
+		JButton button_FiltroTodos = new JButton("Todos");
+		button_FiltroTodos.setBounds(137, 312, 80, 22);
+		contentPane.add(button_FiltroTodos);
 
-		TextField textField = new TextField();
-		textField.setBounds(65, 225, 80, 22);
-		contentPane.add(textField);
+		TextField textField_Avaliacao = new TextField();
+		textField_Avaliacao.setBounds(69, 145, 45, 22);
+		contentPane.add(textField_Avaliacao);
 
-		Label label_2 = new Label("NOME");
-		label_2.setBounds(65, 197, 62, 22);
-		contentPane.add(label_2);
+		TextField textField_Nome = new TextField();
+		textField_Nome.setBounds(32, 225, 91, 22);
+		contentPane.add(textField_Nome);
+
+		JLabel lbl_EatDrink = DefaultComponentFactory.getInstance()
+				.createTitle("Eat and Drink");
+		lbl_EatDrink.setBounds(89, 11, 632, 31);
+		lbl_EatDrink.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_EatDrink.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		contentPane.add(lbl_EatDrink);
+
+		JLabel lbl_Avaliacao = new JLabel("AVALIA\u00C7\u00C3O");
+		lbl_Avaliacao.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_Avaliacao.setBounds(32, 117, 91, 22);
+		contentPane.add(lbl_Avaliacao);
+
+		JLabel lbl_MaiorIgual = new JLabel(">=");
+		lbl_MaiorIgual.setBounds(42, 145, 26, 22);
+		contentPane.add(lbl_MaiorIgual);
+
+		JLabel lbl_Nome = new JLabel("NOME");
+		lbl_Nome.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_Nome.setBounds(32, 198, 91, 22);
+		contentPane.add(lbl_Nome);
 
 	}
 }
