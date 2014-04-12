bb075b1b85257acf985252a38765f0a5504a7a4e
atsoc0ocsav@gmail.com <atsoc0ocsav@gmail.com@2edde95b-6862-c577-c830-67069ed00eb0>
2014-04-12 19:58:56 +0000

    Added Methods to the Controller

--- trunk/src/controlo/CtrlDetalhesEstabelecimento.java
+++ trunk/src/controlo/CtrlDetalhesEstabelecimento.java
@@ -20,8 +20,10 @@ public class CtrlDetalhesEstabelecimento {
 	 * @return establishment with the given ID
 	 */
 	public Estabelecimento consultarDetalhes() {
-
-		return null;
+		//TODO Estabelecimento de Teste.... Falta fazer o verdadeiro ;)
+		return new Estabelecimento("38º44'52.11717\" 9º9'12.40631\"",
+				"Estabelecimento de Teste", "Virá à esquerda e à direira! ",
+				123, "Aberto 24 sobre 24", "Av. das Forças Armadas", 9.5);
 	}
 
 }
