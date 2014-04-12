29ed31ff024780f6a96f072647c5493bc3554151
atsoc0ocsav@gmail.com <atsoc0ocsav@gmail.com@2edde95b-6862-c577-c830-67069ed00eb0>
2014-04-12 19:59:25 +0000

    Added Constructor and Getters

--- src/dados/Estabelecimento.java
+++ src/dados/Estabelecimento.java
@@ -1,5 +1,53 @@
 package dados;
 
 public class Estabelecimento {
+	private String coordenadasGPS;
+	private String designacao;
+	private String formaDeChegar;
+	private int idEstabelecimento;
+	private String informacoesHorario;
+	private String morada;
+	private double rating;
+
+	public Estabelecimento(String coordenadasGPS, String designacao,
+			String formaDeChegar, int idEstabelecimento,
+			String informacoesHorario, String morada, double rating) {
+		super();
+		this.coordenadasGPS = coordenadasGPS;
+		this.designacao = designacao;
+		this.formaDeChegar = formaDeChegar;
+		this.idEstabelecimento = idEstabelecimento;
+		this.informacoesHorario = informacoesHorario;
+		this.morada = morada;
+		this.rating = rating;
+	}
+
+	public String getCoordenadasGPS() {
+		return coordenadasGPS;
+	}
+
+	public String getDesignacao() {
+		return designacao;
+	}
+
+	public String getFormaDeChegar() {
+		return formaDeChegar;
+	}
+
+	public int getIdEstabelecimento() {
+		return idEstabelecimento;
+	}
+
+	public String getInformacoesHorario() {
+		return informacoesHorario;
+	}
+
+	public String getMorada() {
+		return morada;
+	}
+
+	public double getRating() {
+		return rating;
+	}
 
 }
