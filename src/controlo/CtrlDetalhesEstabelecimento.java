a73b617fa7c765defbceeb6861a288dccbfd2995
atsoc0ocsav@gmail.com <atsoc0ocsav@gmail.com@2edde95b-6862-c577-c830-67069ed00eb0>
2014-04-12 18:46:27 +0000

    Added Methods used by GUI

--- src/controlo/CtrlDetalhesEstabelecimento.java
+++ src/controlo/CtrlDetalhesEstabelecimento.java
@@ -1,5 +1,27 @@
 package controlo;
 
+import dados.Estabelecimento;
+
 public class CtrlDetalhesEstabelecimento {
+
+	/**
+	 * Creates the Establishment Details Controller
+	 * 
+	 * @param establishment
+	 *            ID
+	 */
+	public CtrlDetalhesEstabelecimento(int establishmentID) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Return the establishment with the ID given on the constructor method
+	 * 
+	 * @return establishment with the given ID
+	 */
+	public Estabelecimento consultarDetalhes() {
+
+		return null;
+	}
 
 }
