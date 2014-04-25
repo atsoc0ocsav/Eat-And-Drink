18370004df11e89216e2403c86c66fbf0549d760
rnnma@iscte-iul.pt <rnnma@iscte-iul.pt@2edde95b-6862-c577-c830-67069ed00eb0>
2014-04-25 20:30:33 +0000

    Criada a ligação e interação à base de dados. Inicio da criação de uma classe de teste rápido de interação com a base de dados.

---
+++ src/dbc/TestDB.java
@@ -0,0 +1,57 @@
+package dbc;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TestDB {
+
+	public static void main(String[] Args){
+		
+		DBConnection db = null;
+		Boolean works = false;
+		
+		try {
+			db = new DBConnection();
+			works = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		if (works){
+			ResultSet result;
+			
+			db.insert("");
+			
+			result = db.select("");
+			
+			showResult(result);
+			
+			db.delete("");
+			
+			result = db.select("");
+			
+			showResult(result);
+		}
+		
+		
+	}
+
+	private static void showResult(ResultSet result) {
+		
+		try {
+			while(result.next()){
+				try {
+					System.out.println(result.getString(0));
+				} catch (SQLException e) {
+					System.out.println("Falha ao ir buscar cada resultado");
+					e.printStackTrace();
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Falha ao ir buscar o próximo");
+		}
+		
+	}
+	
+}
