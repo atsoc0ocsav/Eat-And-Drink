0cbdca5e12a0773c89d01048aa81c13fb2641591
atsoc0ocsav@gmail.com <atsoc0ocsav@gmail.com@2edde95b-6862-c577-c830-67069ed00eb0>
2014-04-26 20:45:23 +0000

    Added ctrlConsultarEstabelecimentos class (Refactoring)

---
+++ trunk/src/controlo/ctrlConsultarEstabelecimentos.java
@@ -0,0 +1,50 @@
+package controlo;
+
+import java.util.ArrayList;
+
+import dados.Cidade;
+import dados.Estabelecimento;
+import dados.TipoDeEstablecimento;
+import dados.Zona;
+import dbc.DBConnection;
+
+public class ctrlConsultarEstabelecimentos {
+	private DBConnection dbc;
+	private Estabelecimento estabelecimento;
+	private Cidade cidade;
+	private Zona zona;
+	private TipoDeEstablecimento tipoDeEstablecimento;
+	private TipoDeEvento tipoDeEvento;
+	private TipoDePrato tipoDePrato;
+
+	public ctrlConsultarEstabelecimentos() {
+		dbc = new DBConnection();
+		estabelecimento = new Estabelecimento(dbc);
+		cidade = new Cidade(dbc);
+		zona = new Zona(dbc);
+		tipoDeEstablecimento = new TipoDeEstablecimento(dbc);
+		tipoDeEvento = new TipoDeEvento(dbc);
+		tipoDePrato = new TipoDePrato(dbc);
+	}
+
+	public ArrayList<Estabelecimento> consultarEstabelecimentos(String cidade,
+			String zona, String tipo, double rating, String prato,
+			double preco, String evento, boolean foto, String nome) {
+
+		ArrayList<Estabelecimento> listaEstabelecimentos = new ArrayList<Estabelecimento>();
+
+		listaEstabelecimentos = estabelecimento.select(cidade, zona, tipo,
+				rating, prato, preco, evento, foto, nome);
+
+		return listaEstabelecimentos;
+	}
+
+	public ArrayList<Cidade> getCidades() {
+		return cidade.getCidades();
+	}
+	
+	public ArrayList<Zona> getZonas(){
+		return zona.getZonas();
+	}
+
+}
