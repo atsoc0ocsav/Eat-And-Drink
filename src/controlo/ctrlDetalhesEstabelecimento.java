1a4878dd32bf830b99165b18014ac78583b72484
atsoc0ocsav@gmail.com <atsoc0ocsav@gmail.com@2edde95b-6862-c577-c830-67069ed00eb0>
2014-04-26 20:41:33 +0000

    Added ctrlDetalhesEstabelecimento class (Finishing Refactoring)

---
+++ src/controlo/ctrlDetalhesEstabelecimento.java
@@ -0,0 +1,29 @@
+package controlo;
+
+import dados.Estabelecimento;
+
+public class ctrlDetalhesEstabelecimento {
+
+	/**
+	 * Creates the Establishment Details Controller
+	 * 
+	 * @param establishment
+	 *            ID
+	 */
+	public ctrlDetalhesEstabelecimento(int establishmentID) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Return the establishment with the ID given on the constructor method
+	 * 
+	 * @return establishment with the given ID
+	 */
+	public Estabelecimento consultarDetalhes() {
+		//TODO Estabelecimento de Teste.... Falta fazer o verdadeiro ;)
+		return new Estabelecimento("38º44'52.11717\" 9º9'12.40631\"",
+				"Estabelecimento de Teste", "Virá à esquerda e à direira! ",
+				123, "Aberto 24 sobre 24", "Av. das Forças Armadas", 9.5);
+	}
+
+}
