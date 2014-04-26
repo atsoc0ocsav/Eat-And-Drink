00299c6d902c0ea862ceb1c60a6d4fc7e0bba4f3
atsoc0ocsav@gmail.com <atsoc0ocsav@gmail.com@2edde95b-6862-c577-c830-67069ed00eb0>
2014-04-26 16:58:01 +0000

    Organized final String variables

--- src/dbc/DBConnection.java
+++ src/dbc/DBConnection.java
@@ -1,82 +1,88 @@
 package dbc;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
- 
 
 public class DBConnection {
-	private static final String db = "eatdrink";
-	private static final String user = "dba";
-	private static final String password = "sql";
-	private static final String url = "jdbc:sqlanywhere:Tds:localhost:2638?eng=" + db; 
-	
+	private static final String DB = "eatdrink";
+	private static final String USER = "dba";
+	private static final String PASSWORD = "sql";
+	private static final String HOST_NAME = "localhost";
+	private static final String URL = "jdbc:sqlanywhere:Tds:" + HOST_NAME
+			+ ":2638?eng=" + DB;
+	// private static final String url =
+	// "jdbc:sqlanywhere:Tds:localhost:2638?eng=" + db;
+
 	private Connection conn = null;
-	
+
 	// Acho q tou a ter problemas com o porto!
-	public DBConnection(){
+	public DBConnection() {
 		try {
-		conn = DriverManager.getConnection(url,user,password);
+			conn = DriverManager.getConnection(URL, USER, PASSWORD);
 		} catch (SQLException e) {
 			System.out.println("Erro ao estabelecer a ligação.");
 			System.out.println("SQL Exception: " + e.getMessage());
-            System.out.println("SQL State: " + e.getSQLState());
-            System.out.println("Error Code: " + e.getErrorCode());
+			System.out.println("SQL State: " + e.getSQLState());
+			System.out.println("Error Code: " + e.getErrorCode());
 		}
 	}
-	
 
-	//  Metodos estavam começados com maiuscula no relatorio
-	// Coloquei os métodos a receberem uma string mas se calhar podiam so receber os argumentos necesarios
-	
+	// Metodos estavam começados com maiuscula no relatorio
+	// Coloquei os métodos a receberem uma string mas se calhar podiam so
+	// receber os argumentos necesarios
+
 	public ResultSet select(String sql) {
-		// possível chamada a uma função que prepara o commando select apenas recebendo os seus argumentos
-		
+		// possível chamada a uma função que prepara o commando select apenas
+		// recebendo os seus argumentos
+
 		ResultSet resultSet = null;
-		
+
 		try {
 			PreparedStatement statement = conn.prepareStatement(sql);
 			resultSet = statement.executeQuery();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return resultSet;
-		
+
 	}
-	
+
 	// No relatorio e void mas devia retornar um int...
-	public void insert(String sql){
-		// possível chamada a uma função que prepara o commando insert apenas recebendo os seus argumentos
-		
+	public void insert(String sql) {
+		// possível chamada a uma função que prepara o commando insert apenas
+		// recebendo os seus argumentos
+
 		try {
 			PreparedStatement statement = conn.prepareStatement(sql);
 			statement.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.out.println("SQL Exception: " + e.getMessage());
-            System.out.println("SQL State: " + e.getSQLState());
-            System.out.println("Error Code: " + e.getErrorCode());
+			System.out.println("SQL State: " + e.getSQLState());
+			System.out.println("Error Code: " + e.getErrorCode());
 		}
-		
+
 	}
-	
+
 	// No relatorio e void mas devia retornar um int...
-	public void delete(String sql){
-		// possível chamada a uma função que prepara o commando delect apenas recebendo os seus argumentos
-		
+	public void delete(String sql) {
+		// possível chamada a uma função que prepara o commando delect apenas
+		// recebendo os seus argumentos
+
 		try {
 			PreparedStatement statement = conn.prepareStatement(sql);
 			statement.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.out.println("SQL Exception: " + e.getMessage());
-            System.out.println("SQL State: " + e.getSQLState());
-            System.out.println("Error Code: " + e.getErrorCode());
-		}	
-		
+			System.out.println("SQL State: " + e.getSQLState());
+			System.out.println("Error Code: " + e.getErrorCode());
+		}
+
 	}
 
 }
