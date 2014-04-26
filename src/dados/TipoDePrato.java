a389a19ec5519837db671addc96d6fe2f04c9f4e
rnnma@iscte-iul.pt <rnnma@iscte-iul.pt@2edde95b-6862-c577-c830-67069ed00eb0>
2014-04-26 20:33:52 +0000

    Avanços nas classes de dados cidade e zona para ir obter todas os objectos respectivos. Por acabar!

--- src/dados/TipoDePrato.java
+++ src/dados/TipoDePrato.java
@@ -2,14 +2,12 @@ package dados;
 
 import dbc.DBConnection;
 
-public class TipoDeEstablecimento {
+public class TipoDePrato {
 	private DBConnection dbcConnection;
 
-	public TipoDeEstablecimento(DBConnection dbcConnection) {
+	public TipoDePrato(DBConnection dbcConnection) {
 		super();
 		this.dbcConnection = dbcConnection;
 	}
-	
-	
 
 }
