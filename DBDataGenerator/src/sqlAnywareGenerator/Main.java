b46133ae099f6edd09bb250beb67921ceec69088
atsoc0ocsav@gmail.com <atsoc0ocsav@gmail.com@2edde95b-6862-c577-c830-67069ed00eb0>
2014-04-27 05:19:51 +0000

    Added methods to the DBDataGenerator main

--- DBDataGenerator/src/sqlAnywareGenerator/Main.java
+++ DBDataGenerator/src/sqlAnywareGenerator/Main.java
@@ -1,15 +1,30 @@
 package sqlAnywareGenerator;
 
-import java.sql.SQLDataException;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
 
 public class Main {
 	public static void main(String[] args) {
 		try {
 			DBActions dbAction = new DBActions();
 
-			dbAction.addCitiesToDB();
-			dbAction.addZonesToDB();
-		} catch (SQLDataException e) {
+			long time = System.currentTimeMillis();
+			System.out.println("Adding data to database!");
+			System.out.println("---------------------------------------");
+
+			// dbAction.addCitiesToDB();
+			// dbAction.addZonesToDB();
+			// dbAction.addEstablishmentTypesToDB();
+			// dbAction.addWeekDaysToDB();
+			// dbAction.addEventTypeToDB();
+			// dbAction.addMealTypeToDB();
+			// dbAction.addMealsToDB(100000);
+			dbAction.addUsersToDB(2);
+
+			System.out.println("\n---------------------------------------");
+			System.out.println("Task Duration: "
+					+ (System.currentTimeMillis() - time) + " ms");
+		} catch (SQLException | NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
 	}
