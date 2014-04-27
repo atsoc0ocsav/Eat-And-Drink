fee5c658ceacab41090894c9e44a68e910221f95
atsoc0ocsav@gmail.com <atsoc0ocsav@gmail.com@2edde95b-6862-c577-c830-67069ed00eb0>
2014-04-27 00:43:41 +0000

    Added cities generator method to DBActions

--- trunk/DBDataGenerator/src/sqlAnywareGenerator/DBActions.java
+++ trunk/DBDataGenerator/src/sqlAnywareGenerator/DBActions.java
@@ -1,18 +1,36 @@
 package sqlAnywareGenerator;
 
+import java.sql.SQLDataException;
 import java.util.ArrayList;
 
 public class DBActions {
 	public enum Operation_Type {
 		ADD_CITY, ADD_ZONE
 	};
-	
-	private FileParser parser=new FileParser();
 
-	public void addCitiesToDB() {
-		ArrayList<String> cities=(ArrayList<String>)parser.parseFile("/data/Lista de Cidades.txt");
-		for(String string : cities)
-			System.out.println(string);
+	private FileParser parser = new FileParser();
+	private DBConnection dbConnection;
+
+	public DBActions() {
+		this.dbConnection = new DBConnection();
 	}
 
+	public void addCitiesToDB() throws SQLDataException {
+		ArrayList<String> cities = (ArrayList<String>) parser
+				.plainTextFileParser("./data/Lista de Cidades.txt");
+
+		for (String city : cities) {
+			if (dbConnection
+					.executeUpdate("INSERT INTO Cidade(cidade) VALUES ('"
+							+ city + "')") < 0)
+				throw new SQLDataException("Error on city inserting method");
+		}
+
+		System.out.println(cities.size()
+				+ " Cities Added with success to database");
+	}
+
+	public void addZonesToDB() {
+
+	}
 }
