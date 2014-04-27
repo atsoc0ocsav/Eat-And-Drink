03aa894dfa2e492432b7c34ea83b09ff8465d158
atsoc0ocsav@gmail.com <atsoc0ocsav@gmail.com@2edde95b-6862-c577-c830-67069ed00eb0>
2014-04-27 02:00:32 +0000

    Concluded the methods to insert Cidades and Zonas

--- DBDataGenerator/src/sqlAnywareGenerator/DBActions.java
+++ DBDataGenerator/src/sqlAnywareGenerator/DBActions.java
@@ -1,13 +1,11 @@
 package sqlAnywareGenerator;
 
+import java.sql.ResultSet;
 import java.sql.SQLDataException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class DBActions {
-	public enum Operation_Type {
-		ADD_CITY, ADD_ZONE
-	};
-
 	private FileParser parser = new FileParser();
 	private DBConnection dbConnection;
 
@@ -16,21 +14,45 @@ public class DBActions {
 	}
 
 	public void addCitiesToDB() throws SQLDataException {
-		ArrayList<String> cities = (ArrayList<String>) parser
-				.plainTextFileParser("./data/Lista de Cidades.txt");
+		ArrayList<String[]> cities = parser.cvsFileParser(
+				"./data/Lista de Municípios.csv", ";");
 
-		for (String city : cities) {
-			if (dbConnection
-					.executeUpdate("INSERT INTO Cidade(cidade) VALUES ('"
-							+ city + "')") < 0)
-				throw new SQLDataException("Error on city inserting method");
+		for (String[] city : cities) {
+			dbConnection.executeUpdate("INSERT INTO Cidade(Cidade) VALUES ('"
+					+ city[1] + "')");
 		}
 
+		dbConnection.commit();
 		System.out.println(cities.size()
 				+ " Cities Added with success to database");
 	}
 
-	public void addZonesToDB() {
+	public void addZonesToDB() throws SQLException {
+		ArrayList<String[]> zones = parser.cvsFileParser(
+				"./data/Lista de Freguesias.csv", ";");
 
+		ResultSet test = dbConnection
+				.executeQuery("SELECT FIRST idZona FROM Zona ORDER BY idZona DESC");
+
+		int currentID;
+		if (test.next())
+			currentID = test.getInt(1) + 1;
+		else
+			currentID = 0;
+
+		for (String[] zone : zones) {
+			dbConnection
+					.executeUpdate("INSERT INTO Zona(idZona,Cidade,Designação) VALUES ('"
+							+ currentID
+							+ "','"
+							+ zone[0]
+							+ "','"
+							+ zone[1].split("[ (]")[0] + "')");
+			currentID++;
+		}
+		
+		dbConnection.commit();
+		System.out.println(zones.size()
+				+ " Zones Added with success to database");
 	}
 }
