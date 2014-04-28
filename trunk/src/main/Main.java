84892913a92cbcd9d1fadc6cfdba7d21532088a5
atsoc0ocsav@gmail.com <atsoc0ocsav@gmail.com@2edde95b-6862-c577-c830-67069ed00eb0>
2014-04-28 20:07:28 +0000

    Updated main class

--- trunk/src/main/Main.java
+++ trunk/src/main/Main.java
@@ -1,14 +1,11 @@
 package main;
 
 import gui.ecraConsultarEstabelecimentos;
-import controlo.ctrlConsultarEstabelecimentos;
-
 
 public class Main {
 
 	public static void main(String[] args) {
-		ctrlConsultarEstabelecimentos controlador = new ctrlConsultarEstabelecimentos();
-		ecraConsultarEstabelecimentos guiConsulta = new ecraConsultarEstabelecimentos(controlador);
+		new ecraConsultarEstabelecimentos();
 	}
 
 }
