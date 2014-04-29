c722e284a748a2fdd137468bbc37a31dd41b2bdb
atsoc0ocsav@gmail.com <atsoc0ocsav@gmail.com@2edde95b-6862-c577-c830-67069ed00eb0>
2014-04-29 01:36:15 +0000

    Done the controller of the establishment details, to get a establishment by it ID code

--- trunk/src/controlo/ctrlDetalhesEstabelecimento.java
+++ trunk/src/controlo/ctrlDetalhesEstabelecimento.java
@@ -1,7 +1,5 @@
 package controlo;
 
-import java.util.ArrayList;
-
 import dados.Estabelecimento;
 
 public class ctrlDetalhesEstabelecimento {
@@ -12,13 +10,7 @@ public class ctrlDetalhesEstabelecimento {
 	 */
 	public Estabelecimento consultarDetalhesEstabelecimento(int establishmentID) {
 		Estabelecimento establishmentDataObject = new Estabelecimento();
-		ArrayList<Estabelecimento> establishment = establishmentDataObject
-				.select(establishmentID);
-
-		if (establishment != null && establishment.size() > 1)
-			return establishment.get(0);
-		else
-			return null;
+		return establishmentDataObject.select(establishmentID);
 	}
 
 }
