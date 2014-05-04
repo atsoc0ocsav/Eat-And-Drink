9b5e16e1667e644592ce472e2b61050b5f966083
atsoc0ocsav@gmail.com <atsoc0ocsav@gmail.com@2edde95b-6862-c577-c830-67069ed00eb0>
2014-05-04 00:57:53 +0000

    Minnor changes on sysouts on jdialogs

--- trunk/src/interfaceClasses/FotografiasEComentarios.java
+++ trunk/src/interfaceClasses/FotografiasEComentarios.java
@@ -7,14 +7,14 @@ public class FotografiasEComentarios implements InterfaceFotoEComment_Establecim
 	@Override
 	public void showFotografias(int idPrato) {
 		JOptionPane.showMessageDialog(null,
-				"Por implementar a chamada ao módulo de Fotografias e Comentário\n Mostrar fotografias do prato idPrato="
+				"Por implementar a chamada ao módulo de Fotografias e Comentários\n Mostrar fotografias do prato idPrato="
 						+idPrato);
 	}
 
 	@Override
 	public void showComentarios(int idPrato) {
 		JOptionPane.showMessageDialog(null,
-				"Por implementar a chamada ao módulo de Fotografias e Comentário\n Mostrar comentário do prato idPrato="
+				"Por implementar a chamada ao módulo de Fotografias e Comentários\n Mostrar comentário do prato idPrato="
 						+ idPrato);
 	}
 
