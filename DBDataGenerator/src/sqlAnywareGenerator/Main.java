6e7ba2398ee465d44e0fec8e012243adc46250e1
atsoc0ocsav@gmail.com <atsoc0ocsav@gmail.com@2edde95b-6862-c577-c830-67069ed00eb0>
2014-05-04 17:11:35 +0000

    Updated the generated quantities

--- DBDataGenerator/src/sqlAnywareGenerator/Main.java
+++ DBDataGenerator/src/sqlAnywareGenerator/Main.java
@@ -20,17 +20,17 @@ public class Main {
 			dbAction.addWeekDaysToDB();
 			dbAction.addEventTypeToDB();
 			dbAction.addMealTypeToDB();
-			dbAction.addMealsToDB(1000);
+			dbAction.addMealsToDB(2500);
 			dbAction.addUsersToDB(10000);
 			dbAction.addEstablishmentsToDB(1000);
-			dbAction.addFollowersToDB(500);
-			dbAction.addAvailableEventsToDB(2000);
+			dbAction.addFollowersToDB(750);
+			dbAction.addAvailableEventsToDB(3000);
 			dbAction.addEstablishmentScheduleToDB();
-			dbAction.addEstablishmentMenusToDB(10);
+			dbAction.addEstablishmentMenusToDB(15);
 			dbAction.addRecomendedMealsToDB(500);
-			dbAction.addMealsCommentsToDB(500);
-			dbAction.addEstablishmentCommentsToDB(500);
-			dbAction.addMealsPhotographies(500);
+			dbAction.addMealsCommentsToDB(750);
+			dbAction.addEstablishmentCommentsToDB(750);
+			dbAction.addMealsPhotographies(1000);
 
 			System.out.println("\n---------------------------------------");
 			System.out.println("Task Duration: "
