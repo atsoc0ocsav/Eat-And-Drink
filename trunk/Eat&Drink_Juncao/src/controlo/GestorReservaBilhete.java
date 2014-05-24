77d74ff19bff650341d3d1420d72ef303e734f48
ejpgz@iscte-iul.pt <ejpgz@iscte-iul.pt@2edde95b-6862-c577-c830-67069ed00eb0>
2014-05-24 18:52:11 +0000

    Acrescentado as funcionalidades todas do gui ReservaBilhete. [FALTA TESTAR TUDO]

--- trunk/Eat&Drink_Juncao/src/controlo/GestorReservaBilhete.java
+++ trunk/Eat&Drink_Juncao/src/controlo/GestorReservaBilhete.java
@@ -3,14 +3,17 @@ package controlo;
 import java.util.ArrayList;
 
 import dados.Evento;
+import dados.ReservaDeBilhetes;
 
 
 public class GestorReservaBilhete {
 
 	private Evento evento;
+	private ReservaDeBilhetes bilhete;
 
 	public GestorReservaBilhete() {
 		this.evento = new Evento();
+		this.bilhete = new ReservaDeBilhetes();
 	}
 
 	public ArrayList<Evento> getEvento() {
@@ -19,6 +22,19 @@ public class GestorReservaBilhete {
 		array = evento.selectEventoOferecido();
 
 		return array;
+	}
+
+	public ArrayList<ReservaDeBilhetes> getLugares(int id) {
+		ArrayList<ReservaDeBilhetes> array = new ArrayList<>();
+
+		array = bilhete.selectLugares(id);
+
+		return array;
+	}
+
+	public void confirmarBilhete(int idEvento,  int lugar, String estado) {
+		bilhete.updateEstado(idEvento, lugar, estado);
+		
 	}
 
 }
