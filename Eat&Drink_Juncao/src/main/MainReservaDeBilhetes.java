8c9b5fef6790ee6d54f8b59176192cb3fcff70c9
atsoc0ocsav@gmail.com <atsoc0ocsav@gmail.com@2edde95b-6862-c577-c830-67069ed00eb0>
2014-05-30 02:48:17 +0000

    Refactored the generation process while doing some tests

--- Eat&Drink_Juncao/src/main/MainReservaDeBilhetes.java
+++ Eat&Drink_Juncao/src/main/MainReservaDeBilhetes.java
@@ -1,11 +1,12 @@
 package main;
 
 import gui.ReservaBilhete;
+import controlo.GestorReservaBilhete.ConcorrencyLevel;
 
 public class MainReservaDeBilhetes {
-	
+
 	public static void main(String[] args) {
-		new ReservaBilhete();
+		new ReservaBilhete(ConcorrencyLevel.PESSIMIST);
 	}
 
 }
